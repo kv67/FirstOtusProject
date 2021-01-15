eb0a80019e86a0ddaa1bfb89f6e5a4d5106cd713
kv67 <kv67@narod.ru>
2021-01-16 01:42:48 +0300

    Реализована верстка для Main Activity

--- app/src/main/java/kve/ru/firstproject/MainActivity.kt
+++ app/src/main/java/kve/ru/firstproject/MainActivity.kt
@@ -1,10 +1,22 @@
 package kve.ru.firstproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val BLOOD_SPORT = 1
+        const val COCKTAIL = 2
+        const val COMMANDO = 3
+        const val EMMANUELLE = 4
+    }
+
+    private val textViewBloodSport by lazy {
+        findViewById<TextView>(R.id.textViewBloodSport)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
